82a4f8461d3f3440bd68c8d454dcc0a730c9a410
Wana Pichpongsa <wana@pichpongsa.com>
2025-02-15 14:36:23 +0000

    upload test.pdf

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from openai import OpenAI
+import pdfplumber
 import json
 import requests
 
@@ -14,7 +15,10 @@ BASE_URL = "https://api.ragie.ai"
 DOCUMENTS_ENDPOINT = f"{BASE_URL}/documents"
 RETRIEVALS_ENDPOINT = f"{BASE_URL}/retrievals"
 
-def get_datastructure():
+def ragie_get_datastructure() -> list[str]:
+    # PROS: Returns in Markdown format
+    # CONS: OCR is weaker than OpenAI
+
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
@@ -48,102 +52,107 @@ def get_datastructure():
     for i, text in enumerate(ragie_response):
         print(f"Chunk {i} from document:", text, "\n\n\n\n\n")
     return ragie_response
-    """
+
+def openai_get_datastructure(file_path: str) -> dict[str, any]:
+    # This is basically what I want get_datastructure() to return:
+    system_prompt = "return a JSON object that represents the data structure of the document."
+    with open(file_path, "r") as file:
+        query = file.read()
+
     client = OpenAI(api_key=openai_api_key)
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "user", "content": ""}
-        ]
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": query}
+            ]
     )
     print(response.choices[0].message.content)
-    """
 
-def bloodtest_structure():
+def hardcoded_datastructure() -> dict[str, any]:
     # This is basically what I want get_datastructure() to return:
     form_data = {
-    "Lab": {
-        "Lab Number": None,
-        "NHS No.": None,
-        "Hospital No.": None,
-        "Address": None
-    },
-    "Patient Details": {
-        "Surname": None,
-        "Forename": None,
-        "Sex": None,
-        "DOB": None,
-        "Fasting": None,
-        "NHS/PP": None,
-        "Consultant/GP": None,
-        "Ward/Surgery": None
-    },
-    "Clinical Details and Drug Therapy": None,
-    "Requestor": {
-        "Name & Signature": None,
-        "Bleep No.": None
-    },
-    "Sample": {
-        "Date": None,
-        "Time": None,
-        "Urgent": None,
-        "Type": None  # Blood, Urine, CSF
-    },
-    "Tests Requested": {
-        "Biochemistry": {
-            "Brown Top Gel Tube": {
-                "UE": False,
-                "Bone": False,
-                "Liver": False,
-                "Amylase": False,
-                "CRP": False,
-                "Lipids": False,
-                "Thyroid": False,
-                "Glucose Fasting/Random (Yellow Top)": False,
-                "HBA1c (Red Top)": False,
-                "Gent": False,
-                "Digoxin": False,
-                "B12/Folate": False,
-                "Ferritin": False,
-                "Cortisol 9am/Random": False,
-                "Immunoglobulins": False,
-                "Electrophoresis": False
-            }
+        "Lab": {
+            "Lab Number": None,
+            "NHS No.": None,
+            "Hospital No.": None,
+            "Address": None
         },
-        "Haematology": {
-            "Red Top EDTA": {
-                "FBC": False,
-                "ESR": False,
-                "IM": False
+        "Patient Details": {
+            "Surname": None,
+            "Forename": None,
+            "Sex": None,
+            "DOB": None,
+            "Fasting": None,
+            "NHS/PP": None,
+            "Consultant/GP": None,
+            "Ward/Surgery": None
+        },
+        "Clinical Details and Drug Therapy": None,
+        "Requestor": {
+            "Name & Signature": None,
+            "Bleep No.": None
+        },
+        "Sample": {
+            "Date": None,
+            "Time": None,
+            "Urgent": None,
+            "Type": None  # Blood, Urine, CSF
+        },
+        "Tests Requested": {
+            "Biochemistry": {
+                "Brown Top Gel Tube": {
+                    "UE": False,
+                    "Bone": False,
+                    "Liver": False,
+                    "Amylase": False,
+                    "CRP": False,
+                    "Lipids": False,
+                    "Thyroid": False,
+                    "Glucose Fasting/Random (Yellow Top)": False,
+                    "HBA1c (Red Top)": False,
+                    "Gent": False,
+                    "Digoxin": False,
+                    "B12/Folate": False,
+                    "Ferritin": False,
+                    "Cortisol 9am/Random": False,
+                    "Immunoglobulins": False,
+                    "Electrophoresis": False
+                }
             },
-            "Green Top Citrate": {
-                "Clotting Screen": False,
-                "INR": False,
-                "D Dimer": False,
-                "Lupus Anticoag": False
-            }
+            "Haematology": {
+                "Red Top EDTA": {
+                    "FBC": False,
+                    "ESR": False,
+                    "IM": False
+                },
+                "Green Top Citrate": {
+                    "Clotting Screen": False,
+                    "INR": False,
+                    "D Dimer": False,
+                    "Lupus Anticoag": False
+                }
+            },
+            "Immunology": {
+                "Separate Brown Top Tube": {
+                    "ANCA": False,
+                    "ANA": False,
+                    "Anti-Cardiolipin Abs": False,
+                    "Liver Autoantibodies": False,
+                    "TTGA/Coeliac": False,
+                    "Total IgE": False
+                }
+            },
+            "Other Tests": None
         },
-        "Immunology": {
-            "Separate Brown Top Tube": {
-                "ANCA": False,
-                "ANA": False,
-                "Anti-Cardiolipin Abs": False,
-                "Liver Autoantibodies": False,
-                "TTGA/Coeliac": False,
-                "Total IgE": False
-            }
-        },
-        "Other Tests": None
-    },
-    "Anti-Coagulant Therapy": None,
-    "Collected By": None,
-    "Date/Time Received": None,
+        "Anti-Coagulant Therapy": None,
+        "Collected By": None,
+        "Date/Time Received": None,
         "Additional Information": "For details on tests and sample tube requirements, refer to https://esneftpathology.nhs.uk or call 0300 303 5299."
     }
     return form_data
 
-
-def log_query(data_structure: dict):
+def log_instance_of_document(data_structure: dict) -> dict[str, any]:
     """NOTE: I don't know what to do here yet."""
     # Dummy data will be a blood test result doument.
 
@@ -160,7 +169,15 @@ def log_query(data_structure: dict):
     print(response.choices[0].message.content)
 
 def main():
-    get_datastructure()
-
+    # File paths
+    pdf_path = os.path.join(os.getcwd(), "backend", "data", "test.pdf")
+    try:
+        with pdfplumber.open(pdf_path) as pdf:
+            for page in pdf.pages:
+                print(page.extract_text())
+    except FileNotFoundError as e:
+        print(f"Error: Could not find PDF file - {e}")
+    # openai_get_datastructure("data/test.pdf")
+    # log_instance_of_document(hardcoded_datastructure())
 if __name__ == "__main__":
     main()
