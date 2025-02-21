f92111b51835396c085d590979606ce867394782
Wana Pichpongsa <wana@pichpongsa.com>
2025-02-21 10:57:11 +0000

    ui: fix cases table toggle button spacing

--- frontend/src/components/all-cases.tsx
+++ frontend/src/components/all-cases.tsx
@@ -235,7 +235,7 @@ export function CasesTable() {
           ))}
         </TableBody>
       </Table>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="flex items-center justify-end space-x-2 p-4">
         <Button variant="outline" size="sm">
           Previous
         </Button>
